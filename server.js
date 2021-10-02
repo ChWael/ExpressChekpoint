064bb3a35becb77c8b7dd8239e6556a47a0f6316
Wael Chaieb <chaiebwael93@gmail.com>
2021-10-02 23:55:20 +0100

    correction

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const day = new Date().getDay
-const hour = new Date().getHours
+const day = new Date().getDay()
+const hour = new Date().getHours()
 
 app.get('/', (req, res, next)=>{
-    if((day<6)&&(hour>9)&&(hour<17)) res.sendFile(__dirname+'/public/index.html')
+    if((day<=5)&&(hour>9)&&(hour<17)) res.sendFile(__dirname+'/public/index.html')
     else res.sendFile(__dirname+'/public/unfound.html')
 })
 
